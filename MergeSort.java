e4bc8f751cfc96990f26a5b5a5435a40a6aab0d2
Eremiya Rifat <eremia2021@gmail.com>
2022-04-27 19:55:16 +0200

    MergeSort hinzugefügt

--- MergeSort.java
+++ MergeSort.java
@@ -6,71 +6,47 @@ import java.util.Scanner;
 public class MergeSort {
     public static void main(String[] args) {
 
-        /*
-         * ArrayList<Integer> toSort = new ArrayList<Integer>();
-         * try {
-         * Scanner scanner = new Scanner(new File(args[0]));
-         * while (scanner.hasNextInt()) {
-         * String data = scanner.nextLine();
-         * toSort.add(Integer.parseInt(data));
-         * }
-         * 
-         * } catch (FileNotFoundException e) {
-         * System.out.println("Error! File not found.");
-         * e.printStackTrace();
-         * }
-         * 
-         * int[] arr = new int[toSort.size()];
-         * for (int i = 0; i < toSort.size(); i++) {
-         * arr[i] = toSort.get(i);
-         * }
-         * 
-         * long start = System.currentTimeMillis();
-         * 
-         * sort(arr, 0, arr.length);
-         * 
-         * long end = System.currentTimeMillis();
-         * long erg = end - start;
-         * // Das Programm erzeugt keine Ausgabe -1
-         * System.out.println("Operation took " + erg + " ms.");
-         */
-        int[] arr = { 2, 4, 6, 1, 3, 7 };
-        // merge(arr, 0, 3, arr.length);
-        sort(arr, 0, arr.length);
-        for (int a : arr) {
-            System.out.println("sort Array : " + a);
+        ArrayList<Integer> toSort = new ArrayList<Integer>();
+        try {
+            Scanner scanner = new Scanner(new File(args[0]));
+            while (scanner.hasNextInt()) {
+                String data = scanner.nextLine();
+                toSort.add(Integer.parseInt(data));
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error! File not found.");
+            e.printStackTrace();
         }
+
+        int[] arr = new int[toSort.size()];
+        for (int i = 0; i < toSort.size(); i++) {
+            arr[i] = toSort.get(i);
+        }
+
+        long start = System.currentTimeMillis();
+
+        sort(arr);
+
+        long end = System.currentTimeMillis();
+        long erg = end - start;
+        System.out.println("\n sortiertes Feld: ");
+        for (int a : arr) {
+            System.out.println(a);
+        }
+        System.out.println("Operation took " + erg + " ms.");
+
     }
 
-    public static void merge(int[] arr, int p, int q, int r) {
-
-        int[] leftarr = new int[q - p + 1];
-        int[] rightarr = new int[r - q + 1];
-        // + Unendlich am Ende der Arrays reinpacken
-        rightarr[rightarr.length - 1] = (int) Double.POSITIVE_INFINITY;
-        leftarr[leftarr.length - 1] = (int) Double.POSITIVE_INFINITY;
-        // erzeuge Hilfsfelder ( Left )
-        for (int i = 0; i <= q - 1; i++) {
-            leftarr[i] = arr[i + p];
-        }
-        // erzeuge Hilfsfelder ( Right )
-        for (int i = 0; i < rightarr.length - 1; i++) {
-            rightarr[i] = arr[i + q];
-        }
-
-        for (int a : leftarr) {
-            System.out.println("linkes Array : " + a);
-        }
-        for (int a : rightarr) {
-            System.out.println("rechtes Array : " + a);
-        }
+    public static void merge(int[] arr, int[] leftarr, int[] rightarr) {
 
         // Zählvariablen für die beiden Hilfsarrays
         int x = 0;
         int y = 0;
+        int i = 0;
         // Merge
-
-        for (int i = 0; i < arr.length; i++) {
+        // Vergleich zwischen beide Hälfte
+        while (x < leftarr.length && y < rightarr.length) {
             if (leftarr[x] <= rightarr[y]) {
                 arr[i] = leftarr[x];
                 x++;
@@ -78,19 +54,48 @@ public class MergeSort {
                 arr[i] = rightarr[y];
                 y++;
             }
+            i++;
+        }
+        // Wenn ein Feld leer ist soll das andere übernommen werden
+        while (x < leftarr.length) {
+            arr[i] = leftarr[x];
+            x++;
+            i++;
+        }
+        while (y < rightarr.length) {
+            arr[i] = rightarr[y];
+            y++;
+            i++;
         }
 
     }
 
-    public static void sort(int[] arr, int p, int r) {
-
-        if (p < r) // Prüfe ob Rekursion abbricht
+    public static void sort(int[] arr) {
+        int inputLength = arr.length;
+        if (inputLength < 2) // Prüfe ob Rekursion abbricht
         {
-            int q = (p + r) / 2; // Zerlege -> finde Mitte
-            sort(arr, p, q); // Sortiere linkes Teilfeld
-            sort(arr, q + 1, r); // Sortiere rechtes Teilfeld
-            merge(arr, p, q, r); // Füge Teilfelder Zusammen
+            return;
         }
+        int midIndex = inputLength / 2; // Zerlege -> finde Mitte
+
+        int[] leftarr = new int[midIndex];
+        int[] rightarr = new int[inputLength - midIndex]; // Damit auch Ungerade Anzahl an Zahlen richtig sortiert
+                                                          // werden
+
+        // erzeuge Hilfsfelder ( Left )
+        for (int i = 0; i < midIndex; i++) {
+            leftarr[i] = arr[i];
+        }
+        // erzeuge Hilfsfelder ( Right )
+        for (int i = midIndex; i < inputLength; i++) {
+            rightarr[i - midIndex] = arr[i];
+        }
+
+        sort(leftarr); // Sortiere linkes Teilfeld
+        sort(rightarr); // Sortiere rechtes Teilfeld
+
+        merge(arr, leftarr, rightarr); // Füge Teilfelder Zusammen
+
     }
 
 }
